8d1dc9297c96e0306b0cf4f8a1a861a3fae3f5c2
Pooja <pooja.wohlig@gmail.com>
2015-12-29 19:48:23 +0530

    chanegs

--- application/controllers/site.php
+++ application/controllers/site.php
@@ -955,22 +955,21 @@ public function blockCompany()
     $this->load->helper('url');
     $mainurl=$this->config->base_url();
     $exactpath=$mainurl.$companyid.'/index.php/json/blockBackend';
-    echo $exactpath;
     $this->db->query("UPDATE `master_company` SET `isblock`=1 WHERE `id`='$companyid'");
         // create a new cURL resource
-//    $ch = curl_init();
-//
-//    // set URL and other appropriate options
-//    curl_setopt($ch, CURLOPT_URL, "$exactpath");
-//    curl_setopt($ch, CURLOPT_HEADER, 0);
-//
-//    // grab URL and pass it to the browser
-//    curl_exec($ch);
-//
-//    // close cURL resource, and free up system resources
-//    curl_close($ch);
-//     $data["message"] = 1;
-//    $this->load->view("json", $data);
+    $ch = curl_init();
+
+    // set URL and other appropriate options
+    curl_setopt($ch, CURLOPT_URL, "$exactpath");
+    curl_setopt($ch, CURLOPT_HEADER, 0);
+
+    // grab URL and pass it to the browser
+    curl_exec($ch);
+
+    // close cURL resource, and free up system resources
+    curl_close($ch);
+     $data["message"] = 1;
+    $this->load->view("json", $data);
     
 
 }
