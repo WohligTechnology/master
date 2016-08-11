ca3a28c3a2374d03790c26d893a691abc49a785e
Pooja <pthakare33@gmail.com>
2016-08-11 15:24:16 +0530

    fdgsfdg

--- 2/applicationold/models/surveyquestionanswer_model.php
+++ 2/applicationold/models/surveyquestionanswer_model.php
@@ -55,25 +55,26 @@ return $return;
 }
     public function exportsurveyresultcsv($surveyid)
     {
-        $getsurveyname=$this->db->query("SELECT * FROM `hq_conclusionfinalsuggestion` WHERE `id`=1")->row();
-        $surveyname=$getsurveyname->conclusion;
-        $this->load->dbutil();
-            $query=$this->db->query("");
-       $content= $this->dbutil->csv_from_result($query);
-        //$data = 'Some file data';
-$timestamp=new DateTime();
-        $timestamp=$timestamp->format('Y-m-d_H.i.s');
-//        file_put_contents("gs://magicmirroruploads/products_$timestamp.csv", $content);
-//		redirect("http://magicmirror.in/servepublic?name=products_$timestamp.csv", 'refresh');
-        if ( ! write_file("./uploads/$surveyname.csv", $content))
-        {
-             echo 'Unable to write the file';
-        }
-        else
-        {
-            redirect(base_url("uploads/$surveyname.csv"), 'refresh');
-             echo 'File written!';
-        }
+      $this->load->dbutil();
+    	$query=$this->db->query("SELECT `hq_surveyquestionanswer`.`question` as `Question Id`,`hq_surveyquestion`.`content` as `Question`,GROUP_CONCAT(`hq_surveyquestionanswer`.`option`) as `Answers` FROM `hq_surveyquestionanswer`
+       INNER JOIN `hq_surveyquestion` ON `hq_surveyquestion`.`id`=`hq_surveyquestionanswer`.`question`
+       WHERE `hq_surveyquestion`.`survey`='$surveyid'
+       GROUP BY `hq_surveyquestionanswer`.`question`");
+
+         $content= $this->dbutil->csv_from_result($query);
+          //$data = 'Some file data';
+          $timestamp=new DateTime();
+          $timestamp=$timestamp->format('Y-m-d_H.i.s');
+          if ( ! write_file("./uploads/surveyanswer_$timestamp.csv", $content))
+          {
+              // echo 'Unable to write the file';
+          }
+          else
+          {
+                  redirect(base_url("uploads/surveyanswer_$timestamp.csv"));
+              //  echo 'File written!';
+          }
+
     }
 }
 ?>
