2b3cd1c5eb397781d6600a2a944623f00ae64648
Pooja <pthakare33@gmail.com>
2016-09-08 18:09:07 +0530

    db swaap

--- 1/application/config/database.php
+++ 1/application/config/database.php
@@ -52,7 +52,7 @@ $active_record = TRUE;
 $myserver=$_SERVER['SERVER_NAME'];
 $ip = substr($myserver,0,7);
 //if($myserver != "localhost")
-if($myserver != "localhost"  && $ip != "192.168")
+if($myserver == "localhost"  && $ip != "192.168")
 {
   $db['default']['hostname'] = 'localhost';
   $db['default']['username'] = 'root';
--- application/config/database.php
+++ application/config/database.php
@@ -52,7 +52,7 @@ $active_record = TRUE;
 
 $myserver=$_SERVER['SERVER_NAME'];
 
-if($myserver!="localhost")
+if($myserver=="localhost")
 {
     $db['default']['hostname'] = 'localhost';
     $db['default']['username'] = 'root';
