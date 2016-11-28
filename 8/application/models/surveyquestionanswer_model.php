d268c066dd847734004f1ff2c0482114b9bb6e29
Pooja <pthakare33@gmail.com>
2016-11-28 15:31:14 +0530

    gfsjhsfgj

---
+++ 8/application/models/surveyquestionanswer_model.php
@@ -0,0 +1,80 @@
+<?php
+if ( !defined( "BASEPATH" ) )
+exit( "No direct script access allowed" );
+class surveyquestionanswer_model extends CI_Model
+{
+public function create($user,$question,$option)
+{
+$data=array("user" => $user,"question" => $question,"option" => $option);
+$query=$this->db->insert( "hq_surveyquestionanswer", $data );
+$id=$this->db->insert_id();
+if(!$query)
+return  0;
+else
+return  $id;
+}
+public function beforeedit($id)
+{
+$this->db->where("id",$id);
+$query=$this->db->get("hq_surveyquestionanswer")->row();
+return $query;
+}
+function getsinglesurveyquestionanswer($id){
+$this->db->where("id",$id);
+$query=$this->db->get("hq_surveyquestionanswer")->row();
+return $query;
+}
+public function edit($id,$user,$question,$option)
+{$data=array("user" => $user,"question" => $question,"option" => $option);
+$this->db->where( "id", $id );
+$query=$this->db->update( "hq_surveyquestionanswer", $data );
+return 1;
+}
+public function delete($id)
+{
+$query=$this->db->query("DELETE FROM `hq_surveyquestionanswer` WHERE `id`='$id'");
+return $query;
+}
+public function getimagebyid($id)
+{
+$query=$this->db->query("SELECT `image` FROM `hq_surveyquestionanswer` WHERE `id`='$id'")->row();
+return $query;
+}
+public function getemaildropdown()
+{
+$query=$this->db->query("SELECT * FROM `hq_surveyquestionuser`  ORDER BY `id` ASC")->result();
+$return=array(
+"" => ""
+);
+foreach($query as $row)
+{
+  $return[$row->id]=$row->email;
+}
+
+return $return;
+}
+    public function exportsurveyresultcsv($surveyid)
+    {
+      $this->load->dbutil();
+    	$query=$this->db->query("SELECT `hq_surveyquestionanswer`.`question` as `Question Id`,`hq_surveyquestion`.`content` as `Question`,GROUP_CONCAT(`hq_surveyquestionanswer`.`option`) as `Answers` FROM `hq_surveyquestionanswer`
+       INNER JOIN `hq_surveyquestion` ON `hq_surveyquestion`.`id`=`hq_surveyquestionanswer`.`question`
+       WHERE `hq_surveyquestion`.`survey`='$surveyid'
+       GROUP BY `hq_surveyquestionanswer`.`question`");
+
+         $content= $this->dbutil->csv_from_result($query);
+          //$data = 'Some file data';
+          $timestamp=new DateTime();
+          $timestamp=$timestamp->format('Y-m-d_H.i.s');
+          if ( ! write_file("./uploads/surveyanswer_$timestamp.csv", $content))
+          {
+              // echo 'Unable to write the file';
+          }
+          else
+          {
+                  redirect(base_url("uploads/surveyanswer_$timestamp.csv"));
+              //  echo 'File written!';
+          }
+
+    }
+}
+?>
---
+++ 8/application/views/backend/edittestquestion.php
@@ -0,0 +1,76 @@
+<div class="row">
+    <div class="col s12">
+        <h4 class="pad-left-15">Create Test Question</h4>
+    </div>
+    <form class="col s12" method="post" action="<?php echo site_url('site/edittestquestionsubmit');?>" enctype="multipart/form-data">
+         <div class="form-row control-group row-fluid" style="display:none;">
+                <label class="control-label span3" for="normal-field">ID</label>
+                <div class="controls span9">
+                    <input type="text" id="normal-field" class="row-fluid" name="id" value="<?php echo $this->input->get('id');?>">
+                </div>
+            </div>
+            <div class="form-group">
+                <div class="col-sm-8">
+                   <?php $i=0;?>
+                    <?php foreach($table as $row) { ?>
+                     <?php $i++;?>
+                    <input type="checkbox" class="filled-in" id="filled-in-box<?php echo $i; ?>" name="question[]" value="<?php echo $row->id; ?>" <?php if(in_array($row->id,$before)) echo 'checked'; ?>>
+                    <label for="filled-in-box<?php echo $i; ?>"></label>
+                    &nbsp;&nbsp;&nbsp;
+                    <?php echo $row->text; ?>
+                    <br>
+                    <br>
+                   
+                    <?php } ?>
+                </div>
+            </div>
+         
+     
+       
+       
+        <div class="row">
+            <div class="col s12 m6">
+                    <div class=" form-group">
+                <button type="submit" class="btn btn-primary waves-effect waves-light blue darken-4">Save</button>
+<!--                <a href="<?php echo site_url('site/viewtestquestion'); ?>" class="btn btn-secondary waves-effect waves-light  red">Cancel</a>-->
+        </div>
+            </div>
+        </div>
+
+    </form>
+</div>
+<!--
+<section class="panel">
+    <header class="panel-heading">
+        Test Questions
+    </header>
+    <div class="panel-body">
+        <form class="form-horizontal row-fluid" method="post" action="<?php echo site_url('site/edittestquestionsubmit');?>" enctype="multipart/form-data">
+            <div class="form-row control-group row-fluid" style="display:none;">
+                <label class="control-label span3" for="normal-field">ID</label>
+                <div class="controls span9">
+                    <input type="text" id="normal-field" class="row-fluid" name="id" value="<?php echo $this->input->get('id');?>">
+                </div>
+            </div>
+            <div class="form-group">
+                <label class="col-sm-2 control-label">Questions</label>
+                <div class="col-sm-8">
+                    <?php foreach($table as $row) { ?>
+                    <input type="checkbox" id="inlineCheckbox1" name="question[]" value="<?php echo $row->id; ?>" <?php if(in_array($row->id,$before)) echo 'checked'; ?>>&nbsp;
+                    <?php echo $row->text; ?>
+                    <br>
+                    <br>
+                    <?php } ?>
+                </div>
+            </div>
+            <div class="form-group">
+                <label class="col-sm-2 control-label">&nbsp;</label>
+                <div class="col-sm-4">
+                    <button type="submit" class="btn btn-info">Submit</button>
+                </div>
+            </div>
+        </form>
+    </div>
+</section>
+-->
+
